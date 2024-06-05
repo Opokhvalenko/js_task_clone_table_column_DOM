c7a38f21edfb876d82c1e5f618b43a362ecc4e90
Oksana Pokhvalenko <oksanapokhvalenko@gmail.com>
2024-06-05 20:51:53 +0200

    make build

---
+++ index.81f57f79.js
@@ -0,0 +1,2 @@
+var r=document.querySelectorAll("tr"),e=!0,l=!1,t=void 0;try{for(var o,n=r[Symbol.iterator]();!(e=(o=n.next()).done);e=!0){var a=o.value,c=a.cells[1].cloneNode(!0);a.cells[4].before(c)}}catch(r){l=!0,t=r}finally{try{e||null==n.return||n.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.81f57f79.js.map
